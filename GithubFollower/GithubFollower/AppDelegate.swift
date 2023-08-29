57572c4fc4aa741b18556c95f4dbaf47fd492621
Kiều Anh Tuấn <tuanka@rocketstudio.com.vn>
2023-08-29 13:52:07 +0700

    init project

---
+++ GithubFollower/GithubFollower/AppDelegate.swift
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.swift
+//  GithubFollower
+//
+//  Created by Tobi on 29/08/2023.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
---
+++ GithubFollower/GithubFollower/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  GithubFollower
+//
+//  Created by Tobi on 29/08/2023.
+//
+
+import UIKit
+
+import UIKit
+
+final class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
